78ec07c9560769e841ba6eec21f37aca535d59da
Ne0MM <pablomaximiliano260@gmail.com>
2023-04-17 21:19:11 -0300

    SideNavStart

---
+++ src/pages/games.js
@@ -0,0 +1,17 @@
+import styles from '../styles/games.module.css'
+
+export default function games() {
+  return (
+    <div className={styles.container}>
+
+        <header className={styles.gamesHeader}>
+
+        </header>
+
+        <div className={styles.gamesSideNav}>
+
+        </div>
+
+    </div>
+  )
+}
